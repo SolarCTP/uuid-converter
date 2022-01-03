c48a5bc9a81495e835cbf663dbbe2b34aa39b3c1
Solar <50151694+SolarCityPower@users.noreply.github.com>
2022-01-03 11:54:59 +0100

    First commit GG

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import json
+from os import rename
+from sys import argv
+import glob
+import requests
+
+
+
+converter = "http://tools.glowingmines.eu/convertor/uuid/"
+language = "en"
+print(converter)
+
+# Exception Formatter (as in normal Python exception handling)
+
+# Take old UUID and get new UUID from converter, than rename each file
+def convert_uuid(datfile):
+
+    uuid = datfile[-40:-4].strip("-")
+    print("Looking up UUID:", uuid)
+    response = json.loads(requests.get(converter + uuid).content)
+    if "error" in response:
+        invalid_uuids.append(uuid)
+        print(f"Invalid UUID:    {uuid}")
+    else:
+        new_uuid = response["offlinesplitteduuid"]
+        print(f"New UUID:        {new_uuid}")
+        rename(f"{data_folder}/{uuid}.dat", f"{data_folder}/{new_uuid}.dat")
+
+
+data_folder = argv[1]
+print(data_folder)
+
+if not data_folder:
+    print("No folder specified")
+    quit()
+
+datafiles_list = glob.glob(data_folder + "/*.dat")
+invalid_uuids = []
+if len(datafiles_list) == 0:
+    print("The selected folder does not contain any .DAT files")
+else:
+    for dat in datafiles_list:
+        convert_uuid(dat)
+    if not invalid_uuids == []:
+        print("The following UUID's are not valid and weren't updated:\n" + "\n".join(invalid_uuids))
